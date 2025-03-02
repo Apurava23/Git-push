7669c863d67c423d5da7fc1605bbf416c6a135fb
Apurava Anand <erapuravaanand@gmail>
2025-03-02 10:47:35 +0530

    Adding a project thorough local system

---
+++ README.md
@@ -0,0 +1,2 @@
+# new project
+Date 2/02/2025
---
+++ function.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "New raw project created at local system and push into git hub";
+
+}
